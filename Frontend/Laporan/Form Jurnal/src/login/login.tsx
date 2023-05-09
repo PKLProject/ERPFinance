e071edbaa144990845e17659511647aaa40ce4a8
Branzxu <130948965+Branzxu@users.noreply.github.com>
2023-05-09 16:34:34 +0700

    Add files via upload
    membuat tampilan tambah data

---
+++ Frontend/Laporan/Form Jurnal/src/login/login.tsx
@@ -0,0 +1,38 @@
+// import { createSignal, type Component } from 'solid-js';
+// import imageSrc from '../assets/me.png'
+// import imageesrc from '../assets/i.png'
+
+
+// const App: Component = () => {
+//   const [image, setImage] = createSignal(imageSrc);
+//   const [imagee, setimage] = createSignal(imageesrc);
+
+//     return (
+     
+      
+//         <div>
+//         {/* <div>
+     
+      
+//       </div>
+//          <div class = "bg"></div>
+//          <img src={image()} alt="Alternative Text" class="me" elementtiming={''} fetchpriority={'high'}/>
+//          <div class = "kotak"></div>
+//          <img src={imagee()} alt="Alternative Text" class="ai" elementtiming={''} fetchpriority={'high'}/>
+//          <div class = "bunder"></div>
+//          <div class = "bulet"></div>
+//          <div class = "bundar"></div>
+//          <div><input type="text" placeholder="E-mail" class="mail" /></div>
+//          <div><input type="text" placeholder="Password" class="name" /></div>
+//          <div><button class="btn btn-wide bt">Wide</button></div> */}
+//           <div class = "bg"></div>
+//           <div class = "kotak"></div>   
+//           <div class = "bunder"></div>
+//           <img src={image()} alt="Alternative Text" class="me" elementtiming={''} fetchpriority={'high'}/>
+//           <img src={imagee()} alt="Alternative Text" class="oi" elementtiming={''} fetchpriority={'high'}/>
+         
+//         </div>
+//     );
+// };
+
+// export default App;
