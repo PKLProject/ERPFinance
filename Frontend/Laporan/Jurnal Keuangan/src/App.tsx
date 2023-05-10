d00da97e42a9f9ed0297ffc9aa6ef20a723077fd
andreradyatama <130964733+andreradyatama@users.noreply.github.com>
2023-05-10 10:25:37 +0700

    Add files via upload

---
+++ Frontend/Laporan/Jurnal Keuangan/src/App.tsx
@@ -0,0 +1,16 @@
+import type { Component } from 'solid-js';
+
+import logo from './logo.svg';
+import styles from './App.module.css';
+
+const App: Component = () => {
+  return (
+
+
+<h1></h1>
+
+   
+  );
+};
+
+export default App;
